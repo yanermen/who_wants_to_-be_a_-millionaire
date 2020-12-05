e44c6ad85e3e7fa445551ccc1bcb7854f4d0393e
yanermen <yanermen@gmail.com>
2020-12-05 13:17:37 +0400

    test_cases_for_signup_signin

--- register/tests.py
+++ register/tests.py
@@ -1,10 +1,10 @@
 from rest_framework import status
-from rest_framework.test import APITestCase
+from rest_framework.test import APITestCase, APIClient
 
 
 class RegistrationTestCase(APITestCase):
 
-    def test_registration(self):
+    def test_registration(self) -> None:
         data = {"first_name": "testcase",
                 "last_name": "testcase",
                 "username": "testcase",
@@ -13,4 +13,13 @@ class RegistrationTestCase(APITestCase):
 
         response = self.client.post("/api/v1/signup/", data)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+
+class LoginTestCase(APITestCase):
+
+    def test_login(self) -> None:
+        client = APIClient()
+        client.login(username='testcase', password='testcase')
+
+
 
